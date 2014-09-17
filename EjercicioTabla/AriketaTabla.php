795c1960666eb4cc9e194a979bfaeee3d01fc4d3
Ruben <7rufo7@gmail.com>
2014-09-17 10:43:19 +0200

    CORRECTO

--- EjercicioTabla/AriketaTabla.php
+++ EjercicioTabla/AriketaTabla.php
@@ -7,12 +7,14 @@
 
 <?php	
 
+	// Numero de celdas recogido desde el formulario
 	$cantidad = $_POST["numero"];
 
 	echo "<tr><td> Tabla </td></tr>";
 
 	for($i=1; $i <= $cantidad; $i++){
 
+		// Si el numero es multiplo de 10 saltara de linea
 		if($i % 10 == 0){
 			
 			echo "<td><font color='blue'>";
@@ -20,6 +22,7 @@
 			echo "</font></td>";
 			echo "<tr>";
 			echo "</tr>";
+			
 		} else {
 			
 			echo "<td>";
